9541be2b0ee4e7bb5b4540d017667cdcd7b0b9e8
jose-1805 <joseluiscapotemosquera@gmail.com>
2022-10-18 18:39:09 -0500

    Archivo de instalación y correcciones en comandos

---
+++ install.sh
@@ -0,0 +1,12 @@
+app_name="your service name"
+cd /temp
+laravel new $app_name
+mv /temp/$app_name/ /var/www/html
+cd /var/www/html
+mkdir /var/www/html/app/Traits
+mv ApiResponser.php /var/www/html/app/Traits/ApiResponser.php
+composer require laravel/octane
+RUN printf '0\nyes' | php artisan octane:install
+chmod +x ./r
+./ -type f -exec chmod 0777 {} \;
+./ -type d -exec chmod 0777 {} \;
