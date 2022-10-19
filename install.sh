f724b2721f22f34598ff2ba5d11eb67562e00b1c
jose-1805 <joseluiscapotemosquera@gmail.com>
2022-10-18 20:31:08 -0500

    Archivo de instalación y correcciones en comandos #5

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@ app_name="your service name"
 find /var/www/html/ -type f -exec chmod 0777 {} \;
 find /var/www/html/ -type d -exec chmod 0777 {} \;
 composer create-project laravel/laravel /tmp/$app_name
+mv /tmp/$app_name/.* /var/www/html/
 mv /tmp/$app_name/* /var/www/html/
 rm -r /tmp/$app_name/
+rm -r /var/www/html/.git
 mkdir /var/www/html/app/Traits
 mv ApiResponser.php /var/www/html/app/Traits/ApiResponser.php
+mv /var/www/html/es/ /var/www/html/lang/
 composer require laravel/octane
 printf '0\nyes' | php artisan octane:install
 chmod +x /var/www/html/rr
