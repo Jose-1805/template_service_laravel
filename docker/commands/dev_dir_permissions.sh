77eff898f848a35e0f965b75e7d885f096172a7e
jose-1805 <joseluiscapotemosquera@gmail.com>
2022-11-06 13:31:13 -0500

    sh de para comando de asignación de permisos en desarrollo

---
+++ docker/commands/dev_dir_permissions.sh
@@ -0,0 +1,20 @@
+echo "Asignando permisos al directorio ./app"
+find ./app -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./config"
+find ./config -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./database"
+find ./database -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./docker"
+find ./docker -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./lang"
+find ./lang -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./public"
+find ./public -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./resources"
+find ./resources -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./routes"
+find ./routes -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./storage"
+find ./storage -type d -exec chmod 0777 {} \;
+echo "Asignando permisos al directorio ./tests"
+find ./tests -type d -exec chmod 0777 {} \;
---
+++ docker/commands/dev_files_permissions.sh
@@ -0,0 +1,22 @@
+echo "### Asignando permisos a archivos de la carpeta ./app"
+find ./app -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./config"
+find ./config -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./database"
+find ./database -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./docker"
+find ./docker -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./lang"
+find ./lang -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./public"
+find ./public -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./resources"
+find ./resources -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./routes"
+find ./routes -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./storage"
+find ./storage -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta ./tests"
+find ./tests -type f -exec chmod 0777 {} \;
+echo "### Asignando permisos a archivos de la carpeta raíz"
+find ./ -type f -maxdepth 1 -exec chmod 0777 {} \;
