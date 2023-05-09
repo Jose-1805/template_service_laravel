7d780c54074bce30b31e6c2d0c31c0424f804c0b
jose-1805 <joseluiscapotemosquera@gmail.com>
2023-05-09 00:15:52 -0500

    Creación de Validator desde FormRequest con parametro en cunsulta de reglas de validación

--- Traits/FormRequestToValidator.php
+++ Traits/FormRequestToValidator.php
@@ -6,9 +6,21 @@ use Illuminate\Support\Facades\Validator;
 
 trait FormRequestToValidator
 {
-    public function formRequestToValidator(string $form_request_class, array $data)
+    /**
+     * Crea una instancia de Validator a partir de los datos de una clase FormRequest
+     *
+     * Si recibe el parámetro $id, este se envía a la función rules() que se utiliza para
+     * definir las reglas de validación del FormRequest. Por lo tanto agregue el parámetro
+     * en la función rules de su FormRequest public function rules($id = null)
+     *
+     * @param string $form_request_class
+     * @param array $data
+     * @param [type] $id
+     * @return void
+     */
+    public function formRequestToValidator(string $form_request_class, array $data, $id = null)
     {
         $request = new ($form_request_class)();
-        return Validator::make($data, $request->rules(), $request->messages(), $request->attributes());
+        return Validator::make($data, $request->rules($id), $request->messages(), $request->attributes());
     }
 }
